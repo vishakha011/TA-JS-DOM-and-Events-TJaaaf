26706e773f4c80096841063a1af2ab10f0c354f9
vishakha011 <khatadevishakha01@gmail.com>
2020-09-15 12:36:11 +0530

    block-BJaael complete

--- block-BJaael/code/script.js
+++ block-BJaael/code/script.js
@@ -1,23 +1,30 @@
-let boxes = document.querySelector('.boxes');
-// let button2 = document.querySelector('.second');
 
-var colors = [ 'aliceblue', 'antiquewhite', 'blue', 'burlywood', 'chocolate','deepskyblue', 'dimgray', 'hotpink', 'indianred', 'indigo','ivory', 'khaki', 'lavender', 'lightblue', 'lightgreen', 'navy', 'oldlace', 'orange', 'papayawhip', 'pink',
+
+let colors = [ 'aliceblue', 'antiquewhite', 'blue', 'burlywood', 'chocolate','deepskyblue', 'dimgray', 'hotpink', 'indianred', 'indigo','ivory', 'khaki', 'lavender', 'lightblue', 'lightgreen', 'navy', 'oldlace', 'orange', 'papayawhip', 'pink',
 'tomato', 'turquoise', 'violet', 'yellow', 'yellowgreen'];
 
-for(i=0;i<500;i++){ 
-    let randomBox = document.createElement("div");
-    randomBox.classList.add('box')
-    let randomNumber = Math.floor((Math.random() * 100) + 1);     
-    randomBox.append(randomNumber); 
-    boxes.append(randomBox);  
-    } 
+let boxes = document.querySelector('.boxes');
 
-boxes.addEventListener('mousemove', function(){
-    let randomColor = document.querySelector('.box');
-    randomColor.style.backgroundColor = colors[Math.floor(Math.random() * (colors.length - 1))];
-    });
+for(let i = 1; i<=500; i++){
+    let box = document.createElement('div');
+    box.classList.add("box");
+    let randomNumber = Math.floor((Math.random() * 500) + 1); 
+    box.innerText = randomNumber
+    boxes.append(box);
 
-boxes.addEventListener('mousemove', function(){
-    let randomNumbers = document.querySelector('.box');
-    randomNumbers.innerText =  Math.floor((Math.random() * 100) + 1);
-    });
+}
+
+
+function discoGrid (){
+   let randomBox = document.querySelectorAll('.box');
+     randomBox.forEach((element) => {
+        let randomNumber = Math.floor(Math.random()*500 +1);
+        let randomColor = colors[Math.floor(Math.random() * (colors.length - 1))];
+
+        element.innerText = randomNumber;
+        element.style.backgroundColor  = randomColor;
+    })
+}
+
+ 
+ boxes.addEventListener('mousemove', discoGrid) 
