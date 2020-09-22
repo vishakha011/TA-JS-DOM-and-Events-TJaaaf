818441eb8dab83fc3fdee74504107132cb043e67
vishakha011 <khatadevishakha01@gmail.com>
2020-09-22 21:01:27 +0530

    FOrm Validation

--- block-BJaaes/code/script.js
+++ block-BJaaes/code/script.js
@@ -1,17 +1,18 @@
 let form = document.querySelector("form");
+let username = document.querySelector('#username');
+let name = document.querySelector('#name');
+let email = document.querySelector('#email');
+let phone = document.querySelector('#phone');
+let password = document.querySelector('#password');
+let passwordCheck = document.querySelector('#password2');
 
 let userInfo = {};
 
 function handleSubmit(event) {
   event.preventDefault();
-  let username = form.elements.username.value;
-  let name = form.elements.name.value;
-  let email = form.elements.email.value;
-  let phone = form.elements.phone.value
-  let password = form.elements.password.value;
-  let passwordCheck = form.elements['password-check'].value;
+ 
   
-  // console.log(userInfo);
+  // console.log(nameValue);
 
 
   // if(!username){
@@ -20,29 +21,76 @@ function handleSubmit(event) {
   //   alert("Username can't be less than 4 characters");
   // }
   validateForm();
-
   
 }
 
 form.addEventListener("submit", handleSubmit);
 
 function validateForm() {
+  // console.log(nameValue);
 
-  if(username === ""){
-    handleError(username, "Username can't be blank");
-  }else if(username.length < 4){
-    handleError(username, "Username can't be less than 4 characters");
+  // Check username
+  if(username.value === ""){
+    handleError(username, "Username can not be blank");
+  }else if((username.value).length < 4){
+    handleError(username, "Username can not be less than 4 characters");
   }else {
     handleSuccess(username);
   }
 
-  function handleError(input, message){
-    let formControl = input.parentElement;
-    let small = formControl.querySelector('small');
-    small.innerText = message;
-    formControl.className = 'form-control error';
-    
+  // check name
+  if(name.value === "") {
+    handleError(name, "Name can not be blank");
+  }else if(!isNaN(name.value)) {
+    handleError(name, "Can not use numbers as name");
+  }else {
+    handleSuccess(name);
   }
+ 
+  // check email
+  if((email.value).length <= 6) {
+    handleError(email, "Email can not be less than 6 characters");
+  }else {
+    handleSuccess(email);
+  }
+
+ //check phone number
+ if(isNaN(phone.value)) {
+  handleError(phone, "Can not use alphabets");
+ }
+ else if((phone.value).length < 7) {
+  handleError(phone, "Phone number can not be less than 7 digits");
+}else {
+  handleSuccess(phone);
+} 
+
+// check password
+if(password.value === ""){
+  handleError(password, "Password can not be blank");
+}else if((password.value).length < 8) {
+  handleError(password, "Password  can not be less than 7 digits");
+}else {
+  handleSuccess(password);
+}
+
+// confirm password
+// if(passwordCheck.value === ""){
+//   handleError(password2, "Password can not be blank");
+// }
+// }else if(password.value !== passwordCheck.value) {
+//   handleError(confirm, "Password do not match");
+// }
+// else {
+//   handleSuccess(password2);
+// }
+
+function handleError(input, message){
+  let formControl = input.parentElement;
+  let small = formControl.querySelector('small');
+  small.innerText = message;
+  formControl.className = 'form-control error';
+  
+}
 }
 
 
@@ -51,3 +99,4 @@ function handleSuccess(input){
   formControl.className = 'form-control success';
 }
 
+// alert("User Added Successfully!");
