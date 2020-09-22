7f1d7c999f1d9af6b98ba24d84a41ccd62e8ac63
vishakha011 <khatadevishakha01@gmail.com>
2020-09-22 07:27:46 +0530

    Initial Commit

--- block-BJaaes/code/script.js
+++ block-BJaaes/code/script.js
@@ -4,12 +4,50 @@ let userInfo = {};
 
 function handleSubmit(event) {
   event.preventDefault();
-  userInfo.email = form.elements.email.value;
-  userInfo.gender = form.elements.gender.value;
-  userInfo.terms = form.elements.terms.checked;
+  let username = form.elements.username.value;
+  let name = form.elements.name.value;
+  let email = form.elements.email.value;
+  let phone = form.elements.phone.value
+  let password = form.elements.password.value;
+  let passwordCheck = form.elements['password-check'].value;
+  
+  // console.log(userInfo);
 
-  // send data to server
-  console.log(userInfo);
+
+  // if(!username){
+  //   alert("Username can't be empty");
+  // }else if(username.length < 4){
+  //   alert("Username can't be less than 4 characters");
+  // }
+  validateForm();
+
+  
 }
 
 form.addEventListener("submit", handleSubmit);
+
+function validateForm() {
+
+  if(username === ""){
+    handleError(username, "Username can't be blank");
+  }else if(username.length < 4){
+    handleError(username, "Username can't be less than 4 characters");
+  }else {
+    handleSuccess(username);
+  }
+
+  function handleError(input, message){
+    let formControl = input.parentElement;
+    let small = formControl.querySelector('small');
+    small.innerText = message;
+    formControl.className = 'form-control error';
+    
+  }
+}
+
+
+function handleSuccess(input){
+  let formControl = input.parentElement;
+  formControl.className = 'form-control success';
+}
+
